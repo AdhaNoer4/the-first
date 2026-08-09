5ed636dd2e0769f8233dce6ce43b8b24396937ee
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2026-08-09 16:28:05 +0700

    Lesson 4 - Add enemy AI

---
+++ scripts/enemy.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var speed = 100.0
+var player: Node2D
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta):
+	if player:
+		var direction = global_position.direction_to(player.global_position)
+
+		velocity = direction * speed
+
+		move_and_slide()
