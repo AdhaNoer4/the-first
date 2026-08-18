bbabbdca270acc9f2c593205c6fa6b44d87239df
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2026-08-18 11:30:16 +0700

    Lesson 5 - Add player damage system

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var speed = 300.0
+var max_health = 3
+var health = max_health
 
 func _physics_process(delta):
 	
@@ -12,3 +14,12 @@ func _physics_process(delta):
 	velocity = direction * speed
 	
 	move_and_slide()
+
+func take_damage(amount):
+	health -= amount
+	
+	print("Player HP:", health)
+
+func _on_damage_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		take_damage(1)
