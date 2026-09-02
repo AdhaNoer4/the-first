ae87f403860cf6ee916ebfdefdb0181108080216
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2026-09-02 15:16:20 +0700

    Lesson 7 - Add enemy spawner and difficulty scaling

---
+++ scenes/enemy_spawner.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var enemy_scene: PackedScene
+@onready var spawn_points = [
+	$SpawnPoint1,
+	$SpawnPoint2,
+	$SpawnPoint3,
+	$SpawnPoint4
+]
+var spawn_interval = 2.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$SpawnTimer.timeout.connect(spawn_enemy)
+
+func spawn_enemy():
+	var enemy = enemy_scene.instantiate()
+	var spawn_point = spawn_points.pick_random()
+
+	enemy.position = spawn_point.position
+	add_child(enemy)
+	spawn_interval -= 0.05
+	spawn_interval = max(spawn_interval, 0.5)
+
+	$SpawnTimer.wait_time = spawn_interval
